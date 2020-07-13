0e7e41cde7ad29fed547c9ae38821a7c9dfddd9c
DarkWarrior703 <56077342+DarkWarrior703@users.noreply.github.com>
2020-07-13 11:34:15 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Text_Based_RPG.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Text_Based_RPG.dir/main.cpp.obj"
+  "Text_Based_RPG.exe"
+  "Text_Based_RPG.exe.manifest"
+  "Text_Based_RPG.lib"
+  "Text_Based_RPG.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Text_Based_RPG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
